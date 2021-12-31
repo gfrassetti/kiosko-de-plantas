58ee291d750818b62e147e8ab8e87f9b588a57be
Guido Frassetti <78879523+gfrassetti@users.noreply.github.com>
2021-12-31 17:31:25 -0300

    store stylish page
    Se arreglo la grilla de una sola row con brakpoints en la columna del template, se le dio estilos al header y demas

--- js/app.js
+++ js/app.js
@@ -1,12 +1,27 @@
+const items = document.getElementById("items");
+const templateCard = document.getElementById("template-card").content;
+const fragment = document.createDocumentFragment();
+
 document.addEventListener("DOMContentLoaded", () => fetchData());
 
 const fetchData = async () => {
   try {
     const response = await fetch("../js/api.json");
     const data = await response.json();
-    console.log(data);
+    /* console.log(data); */
+    createItem(data);
   } catch (error) {
     alert("Error");
     console.log("Error");
   }
 };
+
+function createItem(data) {
+  data.forEach((item) => {
+    console.log(item);
+    templateCard.querySelector("h2").textContent = item.title;
+    const clone = templateCard.cloneNode(true);
+    fragment.appendChild(clone);
+  });
+  items.appendChild(fragment);
+}
